1a27ecfc46aaec838e28b4cb9f789ffa2f536935
liliangabc <ll917274996@live.com>
2019-08-16 22:27:38 +0800

    增加了2个插件 实现了热更新 构建包体积更小

--- build/rollup.config.prod.js
+++ build/rollup.config.prod.js
@@ -1,13 +1,15 @@
 process.env.NODE_ENV = 'production'
 
 import { uglify } from 'rollup-plugin-uglify'
+import cleanup from 'rollup-plugin-cleanup'
 import configList, { resolveFile } from './rollup.config'
 
 configList.map((config, index) => {
   config.plugins = [
     ...config.plugins,
     ...[
-      uglify()
+      uglify(),
+      cleanup()
     ]
   ]
   return config
